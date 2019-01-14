37d49e0fb8d6afea81ef6845a605a727b017cf35
Brad Richards <brad@kri.ch>
2019-01-14 14:05:13 +0100

    Software Engineering 2019, new examples equals and compareTo (lecture 4)

---
+++ src/ch/fhnw/richards/lecture04/identityExamples/MyClassInt.java
@@ -0,0 +1,29 @@
+package ch.fhnw.richards.lecture04.identityExamples;
+
+public class MyClassInt implements Comparable<MyClassInt> {
+	private final int idAttr;
+
+	public MyClassInt(int idAttr) {
+		this.idAttr = idAttr;
+	}
+
+	@Override
+	public int compareTo(MyClassInt o) {
+		return Integer.compare(this.idAttr, o.idAttr);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || getClass() != obj.getClass()) return false;
+		MyClassInt other = (MyClassInt) obj;
+		return idAttr == other.idAttr;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idAttr;
+		return result;
+	}
+}
---
+++ src/ch/fhnw/richards/lecture04/identityExamples/MyClassString.java
@@ -0,0 +1,36 @@
+package ch.fhnw.richards.lecture04.identityExamples;
+
+/**
+ * This class uses a String as an identity attribute. We implement .equals,
+ * .compareTo and .hashCode methods.
+ */
+public class MyClassString implements Comparable<MyClassString> {
+	private final String idAttr;
+
+	public MyClassString(String idAttr) {
+		this.idAttr = idAttr;
+	}
+
+	@Override
+	public int compareTo(MyClassString o) {
+		return idAttr.compareTo(o.idAttr);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || getClass() != obj.getClass()) return false;
+		MyClassString other = (MyClassString) obj;
+		if (idAttr == null)
+			return (other.idAttr == null);
+		else
+			return (idAttr.equals(other.idAttr));
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idAttr == null) ? 0 : idAttr.hashCode());
+		return result;
+	}
+}
