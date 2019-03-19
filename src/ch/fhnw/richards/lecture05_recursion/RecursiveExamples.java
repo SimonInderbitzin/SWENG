2677bcc6d35c4da4ccb1b7a9f44414f7599147c9
brad <bradley.richards@fhnw.ch>
2019-03-19 10:16:43 +0100

    Added exercise solutions for recursion; simplified examples

--- src/ch/fhnw/richards/lecture05_recursion/RecursiveExamples.java
+++ src/ch/fhnw/richards/lecture05_recursion/RecursiveExamples.java
@@ -13,27 +13,19 @@ public class RecursiveExamples {
 
 	// --- Find string in unsorted array ---
 
-	public static boolean find(String search, String[] values) {
-		return find(search, values.length - 1, values);
-	}
-
-	private static boolean find(String search, int index, String[] values) {
-		if (index < 0) {
+	public static boolean find(String search, String[] values, int pos) {
+		if (pos >= values.length) {
 			return false; // Base case 1
-		} else if (values[index].equals(search)) {
+		} else if (values[pos].equals(search)) {
 			return true; // Base case 2
 		} else { // recursive case
-			return find(search, index - 1, values);
+			return find(search, values, pos + 1);
 		}
 	}
 	
 	// --- Find string in sorted array (binary search) ---
 
-	public static boolean find2(String search, String[] values) {
-		return find2(search, 0, values.length - 1, values);
-	}
-
-	private static boolean find2(String search, int start, int end, String[] values) {
+	public static boolean find2(String search, String[] values, int start, int end) {
 		if (start > end) return false; // Base case 1
 		
 		int middle = (start + end) / 2;
@@ -42,27 +34,21 @@ public class RecursiveExamples {
 		if (compareResult == 0) return true; // Base case 2
 		
 		if (compareResult > 0) { // recursive case 1
-			return find2(search, start, middle - 1, values);
+			return find2(search, values, start, middle - 1);
 		} else { // recursive case 2
-			return find2(search, middle + 1, end, values);
+			return find2(search, values, middle + 1, end);
 		}
 	}	
 
-	// --- Count number of positive values in an array, divide-and-conquer ---
-	// Note: To deal with empty arrays (zero elements), we would need another base case
+	// --- Count number of positive values in an array ---
 
-	public static int countPositive(int[] values) {
-		return countPositive(0, values.length - 1, values);
-	}
-
-	private static int countPositive(int start, int end, int[] values) {
-		// Base case: one element
-		if (start == end) return values[start] > 0 ? 1 : 0;
-		
-		// Inductive case: multiple elements left
-		int middle = (start + end) / 2;
-		return countPositive(start, middle, values) +
-				countPositive(middle+1, end, values);
+	public static int countPositive(int[] values, int pos) {
+		if (pos >= values.length) {
+			return 0; // base case
+		} else {
+			int thisElt = (values[pos] > 0 ? 1 : 0);
+			return thisElt + countPositive(values, pos+1);
+		}
 	}
 
 	// -----------------------------------
